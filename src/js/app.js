d40c2d8f826557b0bf9bc8ad9619d22b72d39085
3ilguun <hi@bilguun.design>
2019-09-21 00:19:06 +0900

    adding autoprefixer

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,3 @@
 'use strict';
 
-import Flickity from 'flickity';
-
-alert('hello world');
-var elem = document.querySelector('.work__img');
-var flkty = new Flickity( elem, {
-  cellAlign: 'left',
-  contain: true
-});
+console.log('Hello world!');
