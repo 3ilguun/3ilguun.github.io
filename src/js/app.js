0783fe476b49582cfc74a0963baadef955ebddfa
3ilguun <hi@bilguun.design>
2019-09-21 01:52:04 +0900

    adding flickity

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,13 @@
 'use strict';
 
+import Flickity from 'flickity';
+
 console.log('Hello world!');
+
+var galleryElems = document.querySelectorAll('.work__img');
+for (var i=0, len = galleryElems.length; i < len; i++) {
+  var galleryElem = galleryElems[i];
+  new Flickity( galleryElem, {
+    pageDots: false
+  })
+}
