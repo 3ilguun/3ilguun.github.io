4fc2b1c9b0668bde8f1183584c6e0b090471cf7a
3ilguun <hi@bilguun.design>
2019-09-29 17:20:53 +0900

    UI update

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,25 @@
 'use strict';
 
-import Flickity from 'flickity';
+// import Flickity from 'flickity';
 
 document.addEventListener("DOMContentLoaded", () => {
-  const galleryElems = document.querySelectorAll('.work__images');
-  const sliderElem = document.querySelector('.work__slides');
-  for(const galleryElem of galleryElems) {
-    new Flickity(galleryElem, {
-      pageDots: false,
-      wrapAround: true,
-      lazyLoad: true,
-      selectedAttraction: 0.2,
-      friction: 1
-    });
-  }
-  if (typeof(sliderElem) != 'undefined' && sliderElem != null) {
-    new Flickity(sliderElem, {
-      cellAlign: 'left',
-      pageDots: false,
-      contain: true,
-      lazyLoad: 4
-    });
-  }
+  // const galleryElems = document.querySelectorAll('.work__images');
+  // const sliderElem = document.querySelector('.work__slides');
+  // for(const galleryElem of galleryElems) {
+  //   new Flickity(galleryElem, {
+  //     pageDots: false,
+  //     wrapAround: true,
+  //     lazyLoad: true,
+  //     selectedAttraction: 0.2,
+  //     friction: 1
+  //   });
+  // }
+  // if (typeof(sliderElem) != 'undefined' && sliderElem != null) {
+  //   new Flickity(sliderElem, {
+  //     cellAlign: 'left',
+  //     pageDots: false,
+  //     contain: true,
+  //     lazyLoad: 4
+  //   });
+  // }
 });
